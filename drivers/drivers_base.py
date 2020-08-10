6d62196f58be1d9c82d00526016908f8d92824ea
Ian Cooper <me@iancooper.name>
2020-08-10 15:42:20 -0600

    fix to DisplayDriver.fill() so it covers the full width on landscape displays

--- drivers/drivers_base.py
+++ drivers/drivers_base.py
@@ -65,7 +65,7 @@ class DisplayDriver(ABC):
     def fill(self, color, fillsize):
         """Slow fill routine"""
         image = Image.new('1', (fillsize, self.height), color)
-        for x in range(0, self.height, fillsize):
+        for x in range(0, self.width, fillsize):
             self.draw(x, 0, image)
 
     def clear(self):
