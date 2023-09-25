c58fdb9b44ddf6c3721e84e1ee16ec7547165e4c
mcarr <136939846+mcarr823@users.noreply.github.com>
2023-09-25 18:35:20 +1000

    CLI flag for setting SPI speed in MHz

--- papertty/drivers/driver_it8951.py
+++ papertty/drivers/driver_it8951.py
@@ -187,14 +187,17 @@ class IT8951(DisplayDriver):
             result = result[0:null_index]
         return result
 
-    def init(self, **kwargs):
+    def init(self, mhz=None, **kwargs):
         GPIO.setmode(GPIO.BCM)
         GPIO.setwarnings(False)
         GPIO.setup(self.RST_PIN, GPIO.OUT)
         GPIO.setup(self.CS_PIN, GPIO.OUT)
         GPIO.setup(self.BUSY_PIN, GPIO.IN)
         self.SPI = spidev.SpiDev(0, 0)
-        self.SPI.max_speed_hz = 2000000
+        if mhz:
+            self.SPI.max_speed_hz = mhz * 1000000
+        else:
+            self.SPI.max_speed_hz = 2000000
         self.SPI.mode = 0b00
 
         # It is unclear why this is necessary but it appears to be. The sample
