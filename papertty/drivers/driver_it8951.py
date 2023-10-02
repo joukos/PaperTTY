df704bd875e14b0cf08590d2947bf3b9e30567da
mcarr <136939846+mcarr823@users.noreply.github.com>
2023-10-02 18:26:47 +1100

    vcom fix

--- papertty/drivers/driver_it8951.py
+++ papertty/drivers/driver_it8951.py
@@ -228,7 +228,9 @@ class IT8951(DisplayDriver):
         # Set to Enable I80 Packed mode.
         self.write_register(self.REG_I80CPCR, 0x0001)
 
-        self.VCOM = kwargs.get('vcom', self.VCOM)
+        vcom = kwargs.get('vcom', None)
+        if vcom:
+            self.VCOM = vcom
             
         if self.VCOM != self.get_vcom():
             self.set_vcom(self.VCOM)
