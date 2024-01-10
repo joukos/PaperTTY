47e4b4c02108f6cfe8d895dc772ae95e8f85e5d8
mcarr823 <136939846+mcarr823@users.noreply.github.com>
2024-01-10 15:43:28 +1100

    Added stdin args to specify rows and cols. This is now required for screens with partial update support

--- README.md
+++ README.md
@@ -386,6 +386,8 @@ Option | Description | Default
 `--portrait` | Enable portrait mode | disabled
 `--nofold` | Disable folding (ie. don't wrap to width) | disabled
 `--spacing` | Set line spacing | `0` 
+`--rows` | Set TTY rows (`--cols` required too) | *no default*
+`--cols` | Set TTY columns (`--rows` required too) | *no default*
 
 
 ```sh
--- papertty/papertty.py
+++ papertty/papertty.py
@@ -1185,8 +1185,10 @@ def scrub(settings, size):
 @click.option('--portrait', default=False, is_flag=True, help='Use portrait orientation', show_default=True)
 @click.option('--nofold', default=False, is_flag=True, help="Don't fold the input", show_default=True)
 @click.option('--spacing', default='0', help='Line spacing for the text, "auto" to automatically determine a good value', show_default=True)
+@click.option('--rows', 'ttyrows', default=None, help='Set TTY rows (--cols required too)')
+@click.option('--cols', 'ttycols', default=None, help='Set TTY columns (--rows required too)')
 @click.pass_obj
-def stdin(settings, font, fontsize, width, portrait, nofold, spacing):
+def stdin(settings, font, fontsize, width, portrait, nofold, spacing, ttyrows, ttycols):
     """Display standard input and leave it on screen"""
     settings.args['font'] = font
     settings.args['fontsize'] = fontsize
@@ -1200,6 +1202,10 @@ def stdin(settings, font, fontsize, width, portrait, nofold, spacing):
             font_width = ptty.font.getsize('M')[0]
             max_width = int((ptty.driver.width - 8) / font_width) if portrait else int(ptty.driver.height / font_width)
             text = ptty.fold(text, width=max_width)
+    if ttyrows:
+        ptty.rows = int(ttyrows)
+    if ttycols:
+        ptty.cols = int(ttycols)
     ptty.showtext(text, fill=ptty.driver.black, portrait=portrait)
 
 
